f6845f048fbe57658c804a402bfcabf3a5c3038e
rkriar <ramneekriar@gmail.com>
2022-09-22 00:29:31 -0400

    Added atomic statements only

--- question2_grocery_bill.pl
+++ question2_grocery_bill.pl
@@ -2,9 +2,9 @@
 % If you only have 2 group members, leave the last space blank
 %
 %%%%%
-%%%%% NAME: 
-%%%%% NAME:
-%%%%% NAME:
+%%%%% NAME: Karanvir Heer
+%%%%% NAME: Ramneek Riar
+%%%%% NAME: Shruti Sharma
 %
 % Add the required atomic propositions and rules in the corresponding sections. 
 % If you put the propositions and rules in the wrong sections, you may lose marks.
@@ -14,22 +14,37 @@
 %%%%% ATOMIC: cost
 % Add the atomic propositions for cost in this section
 
+cost(bread, 4)
+cost(lettuce, 2)
+cost(apple, 1)
+cost(chocolate_bar, 3)
+cost(ginger_ale, 2)
 
 %%%%%  ATOMIC: twoForOneSale
 % Add the atomic propositions for twoForOneSale in this section
 
+twoForOneSale(bread)
+twoForOneSale(apple)
 
 %%%%% ATOMIC: taxable
 % Add the atomic propositions for taxable in this section
 
+taxable(chocolate_bar)
+taxable(ginger_ale)
 
 %%%%% ATOMIC: numPurchased
 % Add the atomic propositions for numPurchased in this section
 
+numPurchased(bread, 2)
+numPurchased(lettuce, 3)
+numPurchased(apple, 6)
+numPurchased(chocolate_bar, 1)
+numPurchased(ginger_ale, 2)
 
 %%%%% ATOMIC: taxRate
 % Add the atomic propositions for taxRate in this section
 
+taxRate(0.13)
 
 %%%%% RULE: costAfterTax
 % Add the rule(s) for costAfterTax in this section
