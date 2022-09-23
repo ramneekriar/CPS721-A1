7910a5970dab6221358ceed2171614c0ef7964f2
Shruti <65316109+sshxrma@users.noreply.github.com>
2022-09-22 20:56:47 -0400

    Update question3_intersection.pl

--- question3_intersection.pl
+++ question3_intersection.pl
@@ -2,9 +2,9 @@
 % If you only have 2 group members, leave the last space blank
 %
 %%%%%
-%%%%% NAME: 
-%%%%% NAME:
-%%%%% NAME:
+%%%%% NAME: Karanvir Heer
+%%%%% NAME: Ramneek Riar
+%%%%% NAME: Shruti Sharma
 %
 % Add the required atomic propositions and rules in the corresponding sections. 
 % If you put the propositions and rules in the wrong sections, you may lose marks.
@@ -13,23 +13,37 @@
 
 %%%%% ATOMIC: clockwise
 % Add the atomic propositions for clockwise (part a) in this section
-
+clockwise(east, north).
+clockwise(south, east).
+clockwise(west, south).
+clockwise(north, west).
 
 %%%%% ATOMIC: counterclockwise
 % Add the atomic propositions for counterclockwise (part a) in this section
-
+counterclockwise(west, north).
+counterclockwise(south, west).
+counterclockwise(east, south).
+counterclockwise(north, east).
 
 %%%%% ATOMIC: reverseDirection
 % Add the atomic propositions for reverseDirection (part a) in this section
-
+reverseDirection(north, south).
+reverseDirection(south, north).
+reverseDirection(west, east).
+reverseDirection(east, west).
 
 %%%%% ATOMIC: facing
 % Add the atomic propositions for facing (part b) in this section
-
+facing(toyota, south).
+facing(nissan, north).
+facing(chevrolet, east).
 
 %%%%% ATOMIC: lightColour
 % Add the atomic propositions for lightColour (part b) in this section
-
+lightColour(north, green).
+lightColour(south, green).
+lightColour(red, east).
+lightColour(red, west).
 
 %%%%% ATOMIC: clockwise
 % Add the atomic propositions for clockwise (part a) in this section
