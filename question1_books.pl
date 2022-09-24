7a43772c3382e89c6bc1eb31e3ef019de226e8e2
Karanvir Heer <karanvirheer@live.com>
2022-09-24 18:13:17 -0400

    Finished Q8 and Q9. Updated KB

--- question1_books.pl
+++ question1_books.pl
@@ -18,13 +18,18 @@ authorOf(chimamanda_ngozi_adichie, half_of_a_yellow_sun).
 authorOf(emily_mandel, station_eleven).
 authorOf(mordecai_richler, barneys_version).
 authorOf(mordecai_richler, the_apprenticeship_of_duddy_kravitz).
-authorOf(madeline_miller, the_song_of_achilles).
 authorOf(naomi_novik, the_golden_enlcaves).
-authorOf(erin_morgenstern, the_starless_sea).
-authorOf(kira_buxton, hollow_kingdom).
 authorOf(naomi_novik, spinning_silver).
+authorOf(naomi_novik, a_deadly_education).
+authorOf(erin_morgenstern, the_starless_sea).
 authorOf(haruki_murakami, hear_the_wind_sing).
 authorOf(haruki_murakami, a_wild_sheep_chase).
+authorOf(james_patterson, kiss_the_girls).
+authorOf(james_patterson, random_case).
+authorOf(test_author, answer_case).
+authorOf(macmillan_author, first_book).
+authorOf(macmillan_author, second_book).
+authorOf(macmillan_author, third_book).
 
 %%%%%  ATOMIC: publishedBy
 % Add the atomic propositions for publishedBy in this section
@@ -33,28 +38,38 @@ publishedBy(knopf, half_of_a_yellow_sun).
 publishedBy(knopf, station_eleven).
 publishedBy(chatto_windus, barneys_version).
 publishedBy(gallery_books, the_apprenticeship_of_duddy_kravitz).
-publishedBy(harper_collins, the_song_of_achilles).
 publishedBy(random_house, the_golden_enlcaves).
-publishedBy(doubleday_books, the_starless_sea).
-publishedBy(grand_central, hollow_kingdom).
 publishedBy(del_ray, spinning_silver).
+publishedBy(del_ray, a_deadly_education).
+publishedBy(doubleday_books, the_starless_sea).
 publishedBy(kodansha_int, hear_the_wind_sing).
 publishedBy(vintage, a_wild_sheep_chase).
+publishedBy(grand_central_books, kiss_the_girls).
+publishedBy(grand_central_books, random_case).
+publishedBy(test_publisher, answer_case).
+publishedBy(macmillan_publishers, first_book).
+publishedBy(macmillan_publishers, second_book).
+publishedBy(macmillan_publishers, third_book).
 
 %%%%% ATOMIC: publicationYear
 % Add the atomic propositions for publicationYear in this section
 
-publicationYear(half_of_a_yellow_sun, 2006).
-publicationYear(station_eleven, 2014).
+publicationYear(first_book, 1900).
+publicationYear(second_book, 1901).
+publicationYear(third_book, 1902).
+publicationYear(hear_the_wind_sing, 1979).
+publicationYear(kiss_the_girls, 1995).
+publicationYear(random_case, 1995).
+publicationYear(answer_case, 1995).
 publicationYear(barneys_version, 1997).
 publicationYear(the_apprenticeship_of_duddy_kravitz, 1999).
-publicationYear(the_song_of_achilles, 2012).
+publicationYear(a_wild_sheep_chase, 2002).
+publicationYear(half_of_a_yellow_sun, 2006).
+publicationYear(station_eleven, 2014).
+publicationYear(spinning_silver, 2018).
 publicationYear(the_golden_enlcaves, 2019).
 publicationYear(the_starless_sea, 2019).
-publicationYear(hollow_kingdom, 2019).
-publicationYear(spinning_silver, 2018).
-publicationYear(hear_the_wind_sing, 1979).
-publicationYear(a_wild_sheep_chase, 2002).
+publicationYear(a_deadly_education, 2020).
 
 %%%%% END
 % DO NOT PUT ANY ATOMIC PROPOSITIONS OR LINES BELOW
